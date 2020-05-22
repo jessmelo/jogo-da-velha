fffcac6be9063bd3816c5b714022a009870dfaf4
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-05-21 21:46:19 -0300

    Create JogoDaVelha.java

---
+++ src/JogoDaVelha.java
@@ -0,0 +1,77 @@
+/*********************************************************************/
+/**   ACH2001 - Introdução à Programação                            **/
+/**   EACH-USP - Primeiro Semestre de 2020                          **/
+/**   Turma 04 - Norton Trevisan Roman                              **/
+/**                                                                 **/
+/**   Terceiro Exercício-Programa                                   **/
+/**                                                                 **/
+/**   Jéssica da Paixão Melo               Nº USP: 10875986         **/
+/**                                                                 **/
+/**   --/--/2020                                                    **/
+/*********************************************************************/
+
+/*
+	Jogo da Velha - programa para verificar o status de um jogo.
+	
+	Lista de Status calculado:
+	0 - Jogo nao iniciado: o tabuleiro esta 'vazio', isto é sem pecas X e O;
+    1 - Jogo encerrado1: o primeiro jogador (que usa as pecas X) a o ganhador;
+    2 - Jogo encerrado2: o segundo jogador (que usa as pecas O) a o ganhador;
+    3 - Jogo encerrado3: empate - todas as casas do tabuleiro estao preenchidas com X e O, mas nenhum dos jogadores ganhou;
+    4 - Jogo ja iniciado e em andamento: nenhuma das alternativas anteriores.	
+*/
+
+public class JogoDaVelha {
+	static final char pecaX = 'X';
+	static final char pecaY = 'O';
+	static final char espacoVazio = ' ';
+
+	/*
+		Determina o status de uma partida de Jogo da Velha
+		
+		Entrada:
+			tabuleiro - matriz 3x3 de caracteres representando uma partida valida de Jogo da Velha
+			
+		Saida:
+			um inteiro contendo o status da partida (valores validos de zero a quatro)
+	*/
+	static int verificaStatus(char[][] tabuleiro) {
+		int status = -1;
+
+		// escreva seu codigo aqui
+				
+		
+		
+		return status;
+	}
+	
+	/*
+		Apenas para seus testes. ISSO SERA IGNORADO NA CORRECAO
+	*/
+	public static void main(String[] args) {
+		// escreva seu codigo (para testes) aqui
+
+		char[][] tab0 = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+		char[][] tab1 = {{'X','X','X'},{'O','O',' '},{' ',' ',' '}};
+		char[][] tab2 = {{'O','X','X'},{'X','O','O'},{' ',' ','O'}};
+		char[][] tab3 = {{'O','X','X'},{'X','O','O'},{'O','X','X'}};
+		char[][] tab4 = {{' ',' ',' '},{'X','O','X'},{' ',' ',' '}};
+
+		System.out.println("Status calculado: " + verificaStatus(tab0));
+		System.out.println("Status esperado para o tabuleiro0: 0\n");
+
+		System.out.println("Status calculado: " + verificaStatus(tab1));
+		System.out.println("Status esperado para o tabuleiro1: 1\n");
+
+		System.out.println("Status calculado: " + verificaStatus(tab2));
+		System.out.println("Status esperado para o tabuleiro2: 2\n");
+		
+		System.out.println("Status calculado: " + verificaStatus(tab3));
+		System.out.println("Status esperado para o tabuleiro1: 3\n");
+		
+		System.out.println("Status calculado: " + verificaStatus(tab4));
+		System.out.println("Status esperado para o tabuleiro4: 4\n");
+		
+
+	}
+}
